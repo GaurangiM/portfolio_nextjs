226469e011acfcc80edc2def52e3ae60bb471964
GaurangiM <manegaurangi89@gmail.com>
2024-05-21 21:47:30 +0200

    Resolve issues with deploy

--- my-portfolio/src/pages/index.js
+++ my-portfolio/src/pages/index.js
@@ -1,14 +1,9 @@
 import Head from "next/head";
-import Hero from '@/components/Hero';
+import Hero from '@/components/hero';
 import About from "@/components/about";
-import Skills from '@/components/Skills';
-import Projects from '@/components/Projects';
-import Contact from '@/components/Contact';
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import Skills from '@/components/skills';
+import Projects from '@/components/projects';
+import Contact from '@/components/contact';
 
 export default function Home() {
   return (
